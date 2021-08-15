667c839d939cf62bfac571383f59e7869ed06e74
ramonavacarasu <vacarasuramona@gmail.com>
2021-08-15 09:25:26 +0300

    CustomerRepository

---
+++ src/main/java/com/amigoscode/testing/customer/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.amigoscode.testing.customer;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends CrudRepository<Customer, UUID> {
+}
